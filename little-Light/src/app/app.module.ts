ee79300e42aa66cf5dd8447d29e25f35ce97833f
Mamello <mmoshoeshoe@webelieve.co.za>
2021-11-04 19:39:22 +0200

    add images

--- little-Light/src/app/app.module.ts
+++ little-Light/src/app/app.module.ts
@@ -2,11 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routingComponents } from './app.routing';
+import { OverviewCardComponent } from './components/overview-card/overview-card.component';
+import { RegisterCtaComponent } from './components/register-cta/register-cta.component';
+import { CardRowComponent } from './components/card-row/card-row.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    OverviewCardComponent,
+    RegisterCtaComponent,
+    CardRowComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- little-Light/src/app/app.routing.ts
+++ little-Light/src/app/app.routing.ts
@@ -5,7 +5,7 @@ import { FormPageComponent } from './pages/form-page/form-page.component';
 import { HomePageSliderComponent } from './image-slider/image-slider.component';
 
 const appRoutes: Routes = [
-    { path: 'home', component:HomePageComponent },
+    { path: '', component:HomePageComponent },
     { path: 'form', component: FormPageComponent},
     { path: 'banner', component: HomePageSliderComponent}
 ]
