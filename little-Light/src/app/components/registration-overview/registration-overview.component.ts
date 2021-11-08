8a82a0e05d1b965e688e4aa1b436fb8fc303373b
Mamello <mmoshoeshoe@webelieve.co.za>
2021-11-08 16:21:14 +0200

    add registration page overview (page not responsive)

---
+++ little-Light/src/app/components/registration-overview/registration-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-overview',
+  templateUrl: './registration-overview.component.html',
+  styleUrls: ['./registration-overview.component.css']
+})
+export class RegistrationOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
