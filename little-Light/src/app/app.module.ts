b93fd101183531ee5ffbc883f1300adcb4f418c6
Neo Jusha <neo.jusha@hellocomputer.com>
2021-11-28 09:01:35 +0200

    Fixing Stlying on all the pages

--- little-Light/src/app/app.module.ts
+++ little-Light/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { CardRowComponent } from './components/card-row/card-row.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
 import { RegistrationOverviewComponent } from './components/registration-overview/registration-overview.component';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { LazyLoadImageModule } from 'ng-lazyload-image';
-// import { MatSliderModule } from '@angular/material/slider';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
+import { MatSliderModule } from '@angular/material/slider';
 import { HttpClientModule } from '@angular/common/http';
 import { ClassroomSliderComponent } from './components/classroom-slider/classroom-slider.component';
 import { BabyclassComponent } from './pages/classrooms/babyclass/babyclass.component';
@@ -19,6 +20,8 @@ import { Preschoolclass01Component } from './pages/classrooms/preschoolclass01/p
 import { Preschoolclass02Component } from './pages/classrooms/preschoolclass02/preschoolclass02.component';
 import { GraderrComponent } from './pages/classrooms/graderr/graderr.component';
 import { GraderComponent } from './pages/classrooms/grader/grader.component';
+import { GalleryPageComponent } from './gallery-page/gallery-page.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -36,15 +39,19 @@ import { GraderComponent } from './pages/classrooms/grader/grader.component';
     Preschoolclass01Component,
     Preschoolclass02Component,
     GraderrComponent,
-    GraderComponent
+    GraderComponent,
+    GalleryPageComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    // NgbModule,
-    // LazyLoadImageModule, 
-    // MatSliderModule, 
-    HttpClientModule
+    NgbModule,
+    LazyLoadImageModule, 
+    MatSliderModule, 
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- little-Light/src/app/app.routing.ts
+++ little-Light/src/app/app.routing.ts
@@ -4,11 +4,16 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 // import { FormPageComponent } from './pages/form-page/form-page.component';
 import { HomePageSliderComponent } from './image-slider/image-slider.component';
 import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
+import { GalleryPageComponent } from './gallery-page/gallery-page.component';
+import { RegisterComponent } from './register/register.component'
 
 const appRoutes: Routes = [
     { path: '', component:HomePageComponent },
     { path: 'registration', component: RegistrationPageComponent},
-    { path: 'banner', component: HomePageSliderComponent}
+    { path: 'banner', component: HomePageSliderComponent},
+    { path: 'gallery', component: GalleryPageComponent},
+    { path: 'contact', component: RegisterComponent},
+
 ]
 
 @NgModule ({
